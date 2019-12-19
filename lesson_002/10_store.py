2ff2f38c575eb73616acfc817ca97fcc7ed9cbaf
Василий Глазов <vhaibl@mail.ru>
2019-12-19 20:37:28 +0300

    complete task 10

--- lesson_002/10_store.py
+++ lesson_002/10_store.py
@@ -40,9 +40,43 @@ lamp_code = goods['Лампа']
 lamps_item = store[lamp_code][0]
 lamps_quantity = lamps_item['quantity']
 lamps_price = lamps_item['price']
-lamps_cost = lamps_quantity * lamps_price
+#lamps_cost = lamps_quantity * lamps_price
 print('Лампа -', lamps_quantity, 'шт, стоимость', lamps_cost, 'руб')
+# ------------
+table_code = goods['Стол']
+table_item = store[table_code][0]
+table_quantity = table_item['quantity']
+table_price = table_item['price']
+table_item2 = store[table_code][1]
+table_quantity2 = table_item2['quantity']
+table_cost = store[goods['Стол']][0]['quantity'] * store[goods['Стол']][0]['price']
+table_cost2 = store[goods['Стол']][1]['quantity'] * store[goods['Стол']][1]['price']
+print('Стол -', table_quantity+table_quantity2, 'шт, стоимость', table_cost+table_cost2, 'руб')
 
+sofa_code = goods['Диван']
+sofa_item = store[sofa_code][0]
+sofa_quantity = sofa_item['quantity']
+sofa_price = sofa_item['price']
+sofa_item2 = store[sofa_code][1]
+sofa_quantity2 = sofa_item2['quantity']
+sofa_cost = store[goods['Диван']][0]['quantity'] * store[goods['Диван']][0]['price']
+sofa_cost2 = store[goods['Диван']][1]['quantity'] * store[goods['Диван']][1]['price']
+print('Диван -', sofa_quantity+sofa_quantity2, 'шт, стоимость', sofa_cost+sofa_cost2, 'руб')
+
+chair_code = goods['Стул']
+chair_item = store[chair_code][0]
+chair_quantity = chair_item['quantity']
+chair_price = chair_item['price']
+chair_item2 = store[chair_code][1]
+chair_quantity2 = chair_item2['quantity']
+chair_item3 = store[chair_code][2]
+chair_quantity3 = chair_item3['quantity']
+
+chair_cost = store[goods['Стул']][0]['quantity'] * store[goods['Стул']][0]['price']
+chair_cost2 = store[goods['Стул']][1]['quantity'] * store[goods['Стул']][1]['price']
+chair_cost3 = store[goods['Стул']][2]['quantity'] * store[goods['Стул']][2]['price']
+
+print('Стул -', chair_quantity + chair_quantity2 +chair_quantity3, 'шт, стоимость', chair_cost+chair_cost2+chair_cost3, 'руб')
 # Вывести стоимость каждого вида товара на складе:
 # один раз распечать сколько всего столов и их общая стоимость,
 # один раз распечать сколько всего стульев и их общая стоимость,
@@ -52,7 +86,7 @@ print('Лампа -', lamps_quantity, 'шт, стоимость', lamps_cost, '�
 # WARNING для знающих циклы: БЕЗ циклов. Да, с переменными; да, неэффективно; да, копипаста.
 # Это задание на ручное вычисление - что бы потом понять как работают циклы и насколько с ними проще жить.
 
-# TODO здесь ваш код
+# TODO что-то голова уже не соображает, как сделать проще
 
 ##########################################################################################
 # ВНИМАНИЕ! После того как __ВСЯ__ домашняя работа сделана и запушена на сервер,         #

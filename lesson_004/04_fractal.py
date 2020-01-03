34c42a40c9007aae286ace4297391aadecdade0d
vhaibl@gmail.com <Kfmzxg7878>
2020-01-03 13:46:04 +0300

    04 done

--- lesson_004/04_fractal.py
+++ lesson_004/04_fractal.py
@@ -20,44 +20,30 @@ import simple_draw as sd
 
 
 def draw_bunches(start_point, angle, length):
-    if length < 10:
+    if length < 3:
         return
     angle1 = angle - 30
     angle2 = angle + 30
-    v1 = sd.get_vector(start_point=start_point, angle=angle2, length=length, width=1)
+    v1 = sd.get_vector(start_point=start_point, angle=angle, length=length, width=1)
     v1.draw(sd.COLOR_GREEN)
-    next_angle2 = angle2 * (1 + (sd.random_number(-40, 40) / 100))          # для
+    next_angle1 = angle1 * (1 + (sd.random_number(-40, 40) / 100))
+    next_angle2 = angle2 * (1 + (sd.random_number(-40, 40) / 100))
     next_point = v1.end_point
-    next_length1 = length * 0.75 * (1 + (sd.random_number(-20, 20) / 100))  # большей
-    draw_bunches(start_point=next_point, angle=next_angle2, length=next_length1)
-
-    v1 = sd.get_vector(start_point=start_point, angle=angle1, length=length, width=1)
-    v1.draw(sd.COLOR_YELLOW)
-    #v2 = sd.get_vector(start_point=start_point, angle=angle2, length=length, width=1)
-    # TODO Тут не нужно два вектора рисовать, достаточно одного
-    # TODO А тк в функции вызывается два раза эта же функция, то и будут нарисованы две ветки
-    # TODO рисуем вектор с текущими параметрами, затем рассчитать новые параметры и передать их в вызовы
-    #v2.draw(sd.COLOR_GREEN)
-    next_angle1 = angle1 * (1 + (sd.random_number(-40, 40) / 100))          # дублирование
-    draw_bunches(start_point=next_point, angle=next_angle1, length=next_length1)
-
-    #next_point2 = v2.end_point
-    #next_length2 = length * 0.75 * (1 + (sd.random_number(-20, 20) / 100))  # рандомности
-    # TODO Что-то большая рандомность почти всю структуру дерева нарушила)
-    draw_bunches(start_point=next_point, angle=next_angle1, length=next_length1)
+    next_length = length * 0.75 * (1 + (sd.random_number(-20, 20) / 100))
+    draw_bunches(start_point=next_point, angle=next_angle1, length=next_length)
+    draw_bunches(start_point=next_point, angle=next_angle2, length=next_length)
 
 
 root_point = sd.get_point(300, 30)
+sd.start_drawing()
 draw_bunches(start_point=root_point, angle=90, length=100)
-
+sd.finish_drawing()
 # Пригодятся функции
 # sd.get_point()
 # sd.get_vector()
 # Возможный результат решения см lesson_004/results/exercise_04_fractal_01.jpg
 
 # можно поиграть -шрифтами- цветами и углами отклонения
-
-# TODO здесь ваш код
 
 # 4) Усложненное задание (делать по желанию)
 # - сделать рандомное отклонение угла ветвей в пределах 40% от 30-ти градусов

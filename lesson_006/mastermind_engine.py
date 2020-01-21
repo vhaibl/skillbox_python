2fb086e96a80b2251a40ca5367d214ed7d8f08a1
vhaibl@gmail.com <Kfmzxg7878>
2020-01-21 18:46:15 +0300

    lesson 006 01 fixed

--- lesson_006/mastermind_engine.py
+++ lesson_006/mastermind_engine.py
@@ -1,10 +1,7 @@
 import random
 
-won = 0
 MAX_SYMBOLS = 4
-
 number = None
-
 
 def make_a_number():
     global number
@@ -15,17 +12,12 @@ def make_a_number():
     return number
 
 
-def guess_a_number(userinput):
-    if number == userinput:  # TODO Вот тут интересный момент. Это частный случай при проверке
-        global won, cow, bull  # TODO Если с его помощью мы избегаем длительных вычислений - то оправдать
-        won = 1  # TODO Такое действие можно...
-        return won
-    else:
-        cow = 0
-        bull = 0  # TODO ...НО. Если мы можем убрать проверку на частный случай и делать вывод на основе
-        # TODO количества посчитанных быков - лучше так и поступить. Код станет меньше и понятнее.
-        for x in range(0, MAX_SYMBOLS):
-            if userinput[x] == number[x]:
-                bull += 1
-            elif userinput[x] in number:
-                cow += 1
+def guess_a_number(user_input):
+    global cow, bull
+    cow = 0
+    bull = 0
+    for x in range(0, MAX_SYMBOLS):
+        if user_input[x] == number[x]:
+            bull += 1
+        elif user_input[x] in number:
+            cow += 1

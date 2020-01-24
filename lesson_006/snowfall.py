868ccb21abb43126c5437b27a816c8330da462ec
bi4ok <bi4ok23@gmail.com>
2020-01-24 11:39:20 +0300

    lesson 006: нужны правки в 02

--- lesson_006/snowfall.py
+++ lesson_006/snowfall.py
@@ -40,27 +40,26 @@ def draw_snowflakes():
 def fallen_snowflakes():
     new_list = []
     global x_list, y_list, size_list
-
     for i, y in enumerate(y_list):
         if y < 30:
             new_list.append(i)
-            point1 = sd.get_point(x_list[i], y_list[i])
-
+            point1 = sd.get_point(x_list[i], y_list[i])  # TODO А зачем тут поинт? Вроде не рисуем ничего
     return new_list
 
 
-def remove_snowflakes():
+def remove_snowflakes():  # TODO Раз new_list сделали локальным, то параметром нужно будет передать список с индексами
     global x_list, y_list, size_list
-    for index in reversed(new_list):
+    for index in reversed(new_list):  # TODO И тут его использовать
         size_list.pop(index)
         y_list.pop(index)
         x_list.pop(index)
 
 
-def new_snowflakes():
+def new_snowflakes():  # TODO А сюда параметром передавать длину списка, который передавали в remove..
     global x_list, y_list, size_list
-    for list_create in new_list:
+    for list_create in new_list:  # TODO Чтобы создавать несколько снежинок
         x_list.append(sd.random_number(10, 600))
         y_list.append(sd.random_number(600, 1200))
         size_list.append(sd.random_number(10, 50))
-        new_list.pop()
+        new_list.pop()  # TODO Это не нужно будет.
+        # TODO Кроме того, не стоит удалять элементы списка, по которому идёт цикл, это приведет к путаннице и ошибкам

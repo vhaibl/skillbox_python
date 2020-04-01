99cc62b8c5d1df813fbf547d5754d4e69ae41afc
Vasiliy Glazov <vhaibl@gmail.com>
2020-04-01 10:55:15 +0300

    lesson 007: 01 step 1 done, step 2 work in progress 03 small fixes

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -7,6 +7,7 @@ import simple_draw as sd
 #  - создание снежинки с нужными параметрами
 #  - отработку изменений координат
 #  - отрисовку
+N = 20
 
 
 class Snowflake:
@@ -39,22 +40,37 @@ while True:
     flake.draw()
     if not flake.can_fall():
         break
-    sd.sleep(0.1)
+    sd.sleep(0.01)
     if sd.user_want_exit():
         break
 
+
 # шаг 2: создать снегопад - список объектов Снежинка в отдельном списке, обработку примерно так:
-# flakes = get_flakes(count=N)  # создать список снежинок
-# while True:
-#     for flake in flakes:
-#         flake.clear_previous_picture()
-#         flake.move()
-#         flake.draw()
-#     fallen_flakes = get_fallen_flakes()  # подчитать сколько снежинок уже упало
+def get_flakes(count=N):
+    global flakes_list
+    flakes_list = []
+    for i in range(count):
+        flakes_list.append(Snowflake())
+    return flakes_list
+fallen = 0
+def get_fallen_flakes():
+    global fallen
+
+    if flake.y >= 0:
+        fallen +=1
+
+flakes = get_flakes(count=N)  # создать список снежинок
+while True:
+    for flake in flakes:
+        flake.clear_previous_picture()
+        flake.move()
+        flake.draw()
+    fallen_flakes = get_fallen_flakes()  # подчитать сколько снежинок уже упало
 #     if fallen_flakes:
 #         append_flakes(count=fallen_flakes)  # добавить еще сверху
-#     sd.sleep(0.1)
-#     if sd.user_want_exit():
-#         break
+    sd.sleep(0.01)
+    print(fallen)
+    if sd.user_want_exit():
+        break
 
 sd.pause()

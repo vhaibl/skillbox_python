c18cc00dc66ceaddcf5db7d04584f8c0cea6fee2
Vasiliy Glazov <vhaibl@gmail.com>
2020-04-23 08:15:52 +0300

    lesson 010: 01: done 02: done 03: done

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -21,5 +21,57 @@
 # - поле емейл НЕ содержит @ и .(точку): NotEmailError (кастомное исключение)
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
+class NotNameError(Exception):
+    pass
 
-# TODO здесь ваш код
+
+class NotEmailError(Exception):
+    pass
+
+
+def validate(line):
+    username, email, age = line.split(' ')
+    age = int(age)
+
+    if username.isalpha() == False:
+        raise NotNameError
+    elif '@' not in email and '.' not in email:
+        raise NotEmailError
+    elif age < 10 or age > 99:
+        raise ValueError('Bad Age')
+
+
+def check_regs(filename):
+    with open(filename, 'r', encoding='utf8') as ff:
+        count = 0
+        bad_regs = open('bad_regs.txt.txt', "w", encoding='utf-8')
+        good_regs = open('good_regs.txt.txt', "w", encoding='utf-8')
+        for line in ff:
+            line = line[:-1]
+            count += 1
+            try:
+                validate(line)
+                good_regs.write(line + '\n')
+            except ValueError as exc:
+                if 'unpack' in exc.args[0]:
+                    valerror = f'Не хватает данных в строке {count}: {line}'
+                    print(valerror)
+                    bad_regs.write(valerror + '\n')
+                else:
+                    ageerror = f'Неправильно указан возвраст в строке {count}: {line}'
+                    print(ageerror)
+                    bad_regs.write(ageerror + '\n')
+            except NotNameError:
+                nameerror = f'Неправильно указано имя в строке {count}: {line}'
+                print(nameerror)
+                bad_regs.write(nameerror + '\n')
+
+            except NotEmailError:
+                emailerror = f'Неправильно указана электронная почта в строке {count}: {line}'
+                print(emailerror)
+                bad_regs.write(emailerror + '\n')
+
+        bad_regs.close()
+
+
+check_regs('registrations.txt')

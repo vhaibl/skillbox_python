7c1b901f6f142c8db02828e54d0ab4b7df1ef865
vhaibl <44583314+vhaibl@users.noreply.github.com>
2020-05-05 09:17:56 +0300

    lesson 011: 01 done 02 done with fixes

--- lesson_011/probe2.py
+++ lesson_011/probe2.py
@@ -4,7 +4,10 @@ class PrimeNumbers:
         self.a = []
         self.n = n
         self.b = 1
-        self.zfilter = zfilter
+#        self.zfilter = zfilter
+        self.checklist = {'1': self.is_pal, '2': self.hasthreenines, '3': self.is_lucky,
+                          '4': self.is_luckynines, '5': self.is_palnines, '6': self.is_luckypal}
+        self.check = self.checklist[zfilter]
 
     def __iter__(self):
         self.b = 1
@@ -21,26 +24,7 @@ class PrimeNumbers:
                     break
             else:
                 self.a.append(self.b)
-                if self.zfilter == '1':
-                    if self.is_pal():
-                        return self.b
-                elif self.zfilter == '2':
-                    if self.hasthreenines():
-                        return self.b
-                elif self.zfilter == '3':
-                    if self.is_lucky():
-                        return self.b
-                elif self.zfilter == '4':
-                    if self.is_lucky() and self.is_pal():
-                        return self.b
-                elif self.zfilter == '5':
-                    if self.is_lucky() and self.hasthreenines():
-                        return self.b
-                elif self.zfilter == '5':
-                    if self.is_pal() and self.hasthreenines():
-                        return self.b
-                else:
-                    return self.b
+                if self.check(): return self.b
 
     def sum_digits(self, numn):
         return sum(map(int, numn))
@@ -50,18 +34,34 @@ class PrimeNumbers:
             return True
 
     def is_lucky(self):
+        self.strnumber = str(self.b)
         center = len(self.strnumber) // 2
         return self.sum_digits(self.strnumber[:center]) == self.sum_digits(self.strnumber[-center:])
 
+    def is_luckynines(self):
+        self.strnumber = str(self.b)
+        center = len(self.strnumber) // 2
+        return self.sum_digits(self.strnumber[:center]) == self.sum_digits(self.strnumber[-center:]) \
+               and '999' in str(self.b)
+
+    def is_luckypal(self):
+        self.strnumber = str(self.b)
+        center = len(self.strnumber) // 2
+        return self.sum_digits(self.strnumber[:center]) == self.sum_digits(self.strnumber[-center:]) \
+               and str(self.b) == str(self.b)[::-1] and int(self.b) > 10
+
     def is_pal(self):
         return str(self.b) == str(self.b)[::-1] and int(self.b) > 10
+
+    def is_palnines(self):
+        return str(self.b) == str(self.b)[::-1] and int(self.b) > 10 and '999' in str(self.b)
 
     def nocheck(digit):
         return True
 
 
-prime_number_iterator = PrimeNumbers(n=100000, zfilter='5')
-print('Фильтры: 1 - Палиндромное, 2 - 999 в номере, 3 - Счастливое, 4 - Счстливе и палиндромное, 5 - Счастливое и 999,'
-      ' 6 - Палиндромное и 999')
+prime_number_iterator = PrimeNumbers(n=100000, zfilter='1')
+print('Фильтры: 1 - Палиндромное, 2 - 999 в номере, 3 - Счастливое, 4 - Счастливое и 999, 5 - Палиндромное и 999,'
+      ' 6 - Палиндромное и Счастлтивое')
 for number in prime_number_iterator:
     print(number)
